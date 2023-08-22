c80446af86e436f158711297ba3c40b73139b602
victorescande <victor.escande@elev.ga.ntig.se>
2023-08-22 09:37:01 +0200

    uu

---
+++ najs.py
@@ -0,0 +1,34 @@
+print ("hello world")
+print('"hello world"')
+print("""
+lorum ipsum
+lorum ipsum
+lorum ipsum
+lorum ipsum
+lorum ipsum
+lorum ipsum
+lorum ipsum
+lorum ipsum
+lorum ipsum
+lorum ipsum
+
+""")
+
+
+print ("hej" "\nhej")
+
+print("hej" + " hej")
+
+
+
+x = input("Hej och välkommen vad heter du: ")
+print("hej" + x,"!")
+lol = input("vad är din favoritfärg? ")
+
+age = input("\nhur gammal är du?")
+
+print(x)
+print(age)
+
+print("hej", x, "du är", age, "år, och du gillar färgen", lol)
+
